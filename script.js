d02268c6b7380f84d019e8d1dbc7d771bb11004c
creative-0109 <khushipatil09013@gmail.com>
2026-07-17 18:19:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Portfolio Loaded Successfully 🚀");
+var typed = new Typed("#typing", {
+    strings: [
+        "B.Sc.(CA) Student",
+        "Aspiring Web Developer",
+        "Creative Learner",
+        "Future Software Developer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1500,
+    loop: true
+});
